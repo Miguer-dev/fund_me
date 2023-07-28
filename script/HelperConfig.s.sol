072a3f9c67beaf2910a43ebd9d04dc1aaea64d8e
Miguel Martinez <miguerdev@gmail.com>
2023-07-28 00:04:19 -0400

    Add scripts for deploy contracts

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0 <0.9.0;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig {
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) activeNetworkConfig = getSepoliaEthConfig();
+        else if(block.chainid == 1) activeNetworkConfig = getEthereumEthConfig();
+        else activeNetworkConfig = getAnvilEthConfig();
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return sepoliaConfig;
+    }
+
+    function getEthereumEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory anvilConfig = NetworkConfig(address(0));
+        return anvilConfig;
+    }
+}
