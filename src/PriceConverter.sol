62e4db99317e53a5a3c3238d6445d02aea3f1866
Miguel Martinez <miguerdev@gmail.com>
2023-07-30 02:41:17 -0400

    Update code style

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -7,7 +7,7 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/Ag
 // Why not an interface?
 library PriceConverter {
     // We could make this public, but then we'd have to deploy it
-    function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
+    function _getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
         // Sepolia ETH / USD Address
         // https://docs.chain.link/data-feeds/price-feeds/addresses
         (, int256 answer,,,) = priceFeed.latestRoundData();
@@ -16,8 +16,8 @@ library PriceConverter {
     }
 
     // 1000000000
-    function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
-        uint256 ethPrice = getPrice(priceFeed);
+    function _getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
+        uint256 ethPrice = _getPrice(priceFeed);
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         // the actual ETH/USD conversion rate, after adjusting the extra 0s.
         return ethAmountInUsd;
