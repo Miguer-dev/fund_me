8f26fa29d15c4bee14944064acf48fab11e3ed38
Miguel Martinez <miguerdev@gmail.com>
2023-07-30 19:02:15 -0400

    Update interaction and interaction tests

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,38 +1,36 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
-import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
 contract FundFundMe is Script {
-    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 SEND_VALUE = 0.1 ether;
 
-    function fundFundMe(address mostRecentDeploy) public {
-        FundMe(payable(mostRecentDeploy)).fund{value: SEND_VALUE}();
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
         console.log("Funded FundMe with %s", SEND_VALUE);
     }
 
     function run() external {
-        address mostRecentDeploy = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
-        vm.startBroadcast();
-        fundFundMe(mostRecentDeploy);
-        vm.stopBroadcast();
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        fundFundMe(mostRecentlyDeployed);
     }
 }
 
 contract WithdrawFundMe is Script {
-    uint256 constant SEND_VALUE = 0.1 ether;
-
-    function withdrawFundMe(address mostRecentDeploy) public {
-        FundMe(payable(mostRecentDeploy)).withdraw();       
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+        console.log("Withdraw FundMe balance!");
     }
 
     function run() external {
-        address mostRecentDeploy = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
-        vm.startBroadcast();
-        withdrawFundMe(mostRecentDeploy);
-        vm.stopBroadcast();
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        withdrawFundMe(mostRecentlyDeployed);
     }
 }
--- test/integration/InteractionsTest.t.sol
+++ test/integration/InteractionsTest.t.sol
@@ -11,22 +11,21 @@ contract InteractionsTest is Test {
     DeployFundMe deployFundMe;
 
     address TEST_USER = makeAddr("user"); //create an address for test
-    uint256 constant SEND_VALUE = 0.1 ether;
-    uint256 constant STARTTING_BALANCE = 10 ether;
-    uint8 constant GAS_PRICE = 1;
+    uint256 constant STARTING_BALANCE = 10 ether;
 
     function setUp() external {
-        deployFundMe = new DeployFundMe();
-        fundMe = deployFundMe.run();
-        vm.deal(TEST_USER, STARTTING_BALANCE);
+        DeployFundMe deployer = new DeployFundMe();
+        fundMe = deployer.run();
+        vm.deal(TEST_USER, STARTING_BALANCE);
     }
 
-    function testUserCanFundInteractions() public{
+    function testUserCanFundAndOwnerWithdraw() public {
         FundFundMe fundFundMe = new FundFundMe();
-        hoax(TEST_USER,STARTTING_BALANCE);
         fundFundMe.fundFundMe(address(fundMe));
 
-        address funder = fundMe.getfunders(0);
-        assertEq(TEST_USER, funder);
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        assert(address(fundMe).balance == 0);
     }
 }
